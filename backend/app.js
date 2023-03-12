a00dbbcc08362d8265c816b48a46f8e88c43ec60
Raúl Marrero Quevedo <ramaque01@gmail.com>
2023-03-12 17:12:01 +0000

    testing sequelize connection

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,21 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
+import sequelize from "./db/connection.js";
 
 dotenv.config();
 const app = express();
 
 const { PORT } = process.env;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', async (req, res) => {
+  try {
+    await sequelize.authenticate();
+    res.send('Connection has been established successfully.');
+  } catch (error) {
+    res.send(`Unable to connect to the database: ${error.message}`);
+  }
 })
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)
+  console.log(`Example app listening on port http://localhost:${PORT}`)
 })
