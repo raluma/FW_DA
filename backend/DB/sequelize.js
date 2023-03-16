e6a00e91c94d07dec3aef479e7d10d151da86926
Raúl Marrero Quevedo <ramaque01@gmail.com>
2023-03-16 21:14:56 +0000

    synchronizing models to db

--- backend/DB/sequelize.js
+++ backend/DB/sequelize.js
@@ -1,5 +1,5 @@
 import { Sequelize } from 'sequelize';
-import { sequelize, user, event, exam } from './models';
+import { user, event, exam } from './models.js';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
@@ -9,10 +9,14 @@ const { DIALECT, HOST, USERNAME, PASSWORD, DATABASE } = process.env;
 export const sequelize = new Sequelize(DATABASE, USERNAME, PASSWORD, {
     host: HOST,
     dialect: DIALECT,
-    login: true
+    logging: false
 });
 
 export const User = sequelize.define('User', user);
 export const Event = sequelize.define('Event', event);
 export const Exam = sequelize.define('Exam', exam);
+
+await User.sync({ alter: true });
+await Event.sync({ alter: true });
+await Exam.sync({ alter: true });
 
