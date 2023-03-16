f9e05a418d30e34b63a940453b307ffa2d38e064
Raúl Marrero Quevedo <ramaque01@gmail.com>
2023-03-16 21:12:49 +0000

    creating login y signup endpoints

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
-import { sequelize, User } from "./DB/sequelize.js";
+import { getUser, setUser } from './DB/userQueries.js';
 
 dotenv.config();
 const app = express();
@@ -8,7 +8,31 @@ const app = express();
 const { PORT } = process.env;
 
 app.get('/', async (req, res) => {
-    res.send(User === sequelize.models.User);
+  res.status(200).send("testing API");
+})
+
+app.post('/login', async (req, res) => {
+  const user = await getUser(req.query.login, req.query.password);
+
+  if (user === null) {
+    res.status(404).send();
+  } else {
+    res.status(200).send();
+  }
+})
+
+app.post('/signup', async (req, res) => {
+  const user = await setUser (
+    req.query.username, 
+    req.query.email,
+    req.query.password
+  );
+
+  if (user === false) {
+    res.status(404).send();
+  } else {
+    res.status(200).send();
+  }
 })
 
 app.listen(PORT, () => {
