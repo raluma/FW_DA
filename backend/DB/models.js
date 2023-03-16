eec0e8ec7181d1b881de1a5336141d0f9007a3a1
Raúl Marrero Quevedo <ramaque01@gmail.com>
2023-03-16 18:30:23 +0000

    creating models like user, event or exam

---
+++ backend/DB/models.js
@@ -0,0 +1,76 @@
+import { DataTypes } from 'sequelize';
+
+export const user = {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    }
+}
+
+export const event = {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    date: {
+        type: DataTypes.DATE, 
+        allowNull: false,
+        defaultValue: DataTypes.NOW
+    },
+    short_desc: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    desc: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        unique: true
+    },
+    url_img: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    tag: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    }
+}
+
+export const exam = {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    url_doc: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    url_attachment: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    event_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        unique: true
+    }
+}
+
