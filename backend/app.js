d8518a3dae2c056491f954d0a758b774a0c09eef
Raúl Marrero Quevedo <ramaque01@gmail.com>
2023-03-17 22:40:16 +0000

    creating getEvent and beggining getEvents

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
-import { getUser, setUser } from './DB/userQueries.js';
+import { getUser, createUser } from './DB/userQueries.js';
+import { getEvents, getEvent } from './DB/eventQueries.js';
 
 dotenv.config();
 const app = express();
 
 const { PORT } = process.env;
+
+const existError = (obj) => {
+  return obj instanceof Object && obj['error'] !== undefined;
+}
 
 app.get('/', async (req, res) => {
   res.status(200).send("testing API");
@@ -14,24 +19,49 @@ app.get('/', async (req, res) => {
 app.post('/login', async (req, res) => {
   const user = await getUser(req.query.login, req.query.password);
 
-  if (user === null) {
-    res.status(404).send();
+  if (existError(user)) {
+    res.status(404).send(user);
   } else {
-    res.status(200).send();
+    res.status(200).send(user);
   }
 })
 
 app.post('/signup', async (req, res) => {
-  const user = await setUser (
+  const user = await createUser (
     req.query.username, 
     req.query.email,
     req.query.password
   );
 
-  if (user === false) {
-    res.status(404).send();
+  if (existError(user)) {
+    res.status(404).send(user);
   } else {
     res.status(200).send();
+  }
+})
+
+app.post('/getEvents', async (req, res) => {
+  const events = await getEvents();
+
+  if (existError(events)) {
+    res.status(404).send(events);
+  } else {
+    res.status(200).send(events);
+  }
+})
+
+app.post('/getEvent', async (req, res) => {
+  const event = await getEvent (
+    req.query.date, 
+    req.query.time,
+    req.query.short_desc,
+    req.query.user_id
+  );
+
+  if (existError(event)) {
+    res.status(404).send(event);
+  } else {
+    res.status(200).send(event);
   }
 })
 
