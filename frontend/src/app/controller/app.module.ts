53395e59edcc52776402379f3c963c11c2d7943d
Raúl Marrero Quevedo <ramaque01@gmail.com>
2023-03-18 12:33:54 +0000

    adding components to declarations

--- frontend/src/app/controller/app.module.ts
+++ frontend/src/app/controller/app.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './pages/app.component';
+import { AppComponent } from './app.component';
+import { Calendar } from './components/calendar';
+import { Home } from './pages/home';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Home,
+    Calendar
   ],
   imports: [
     BrowserModule,
