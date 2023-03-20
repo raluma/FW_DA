f744df9a8ed54235bc9baa9190dbc7b0d6cf6ba5
Raúl Marrero Quevedo <ramaque01@gmail.com>
2023-03-20 17:41:05 +0000

    creating session variables

--- frontend/src/app/controller/app.component.ts
+++ frontend/src/app/controller/app.component.ts
@@ -4,4 +4,8 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: '../view/app.component.html'
 })
-export class AppComponent { }
+export class AppComponent {
+  authUser = "";
+  password = "";
+  loged = false;
+}
