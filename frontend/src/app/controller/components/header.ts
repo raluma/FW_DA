a2a327237599a77ca1c88e5795fbbeec51468f61
Raúl Marrero Quevedo <ramaque01@gmail.com>
2023-03-20 23:39:05 +0000

    moving functions from header to loginForm

--- frontend/src/app/controller/components/header.ts
+++ frontend/src/app/controller/components/header.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
 import { faUser, faRightToBracket } from '@fortawesome/free-solid-svg-icons';
 import { AppComponent } from '../app.component';
 
@@ -13,29 +12,14 @@ import { AppComponent } from '../app.component';
 
     showModal = false;
 
-    form = new FormGroup(
-      {
-        authUser: new FormControl(),
-        password: new FormControl()
-      }
-    )
-
-    login(e: Event) {
-      e.preventDefault();
-      console.log(this.form.value)
-      this.showModal = false;
-
-      // this.http.post("http://localhost:3000/getEvents?login=Ralu&password=1234", null)
-      //   .subscribe(data => {
-          
-      // });
-    }
-
     toggleModal(){
       this.showModal = !this.showModal;
     }
 
-    closeSession() {
+    logout() {
+      this.authUser = "";
+      this.password = "";
       this.loged = false;
+      this.showModal = false;
     }
   }
