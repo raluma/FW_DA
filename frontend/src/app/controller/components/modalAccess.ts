73f483569c4a94fe1cd93ee9361300b5a5b2d315
Raúl Marrero Quevedo <ramaque01@gmail.com>
2023-03-21 19:16:26 +0000

    delete files

--- frontend/src/app/controller/components/modalAccess.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-import { Header } from './header';
-
-@Component({
-    selector: 'modalAccess',
-    templateUrl: '../../view/components/modalAccess.html'
-  })
-  export class ModalAccess extends Header { }
