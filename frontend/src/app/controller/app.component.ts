2885cb02724c337b5549cfbfcb35a3b0bef3e93c
Raúl Marrero Quevedo <ramaque01@gmail.com>
2023-03-21 18:11:14 +0000

    adding httpClient to appComponent

--- frontend/src/app/controller/app.component.ts
+++ frontend/src/app/controller/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: '../view/app.component.html'
 })
 export class AppComponent {
+  http = inject(HttpClient);
+
   authUser = "";
   password = "";
   loged = false;
