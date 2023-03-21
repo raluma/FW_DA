43db0ac8206cff57c7f24db6afe4bbfe948c5989
Raúl Marrero Quevedo <ramaque01@gmail.com>
2023-03-21 18:13:36 +0000

    creating modalAccess component

---
+++ frontend/src/app/controller/components/modalAccess.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import { Header } from './header';
+
+@Component({
+    selector: 'modalAccess',
+    templateUrl: '../../view/components/modalAccess.html'
+  })
+  export class ModalAccess extends Header { }
