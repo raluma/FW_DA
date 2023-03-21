7fbce1a75ddc8e3028f46c8bf54d1ebbaac3050a
Raúl Marrero Quevedo <ramaque01@gmail.com>
2023-03-21 18:12:39 +0000

    creating header component

--- frontend/src/app/controller/components/header.ts
+++ frontend/src/app/controller/components/header.ts
@@ -1,19 +1,36 @@
 import { Component } from '@angular/core';
+import { Home } from '../pages/home';
+import { FormControl, FormGroup } from '@angular/forms';
 import { faUser, faRightToBracket } from '@fortawesome/free-solid-svg-icons';
-import { AppComponent } from '../app.component';
 
 @Component({
     selector: 'header',
     templateUrl: '../../view/components/header.html'
   })
-  export class Header extends AppComponent {
-    faUser = faUser;
-    faRightToBracket = faRightToBracket;
-
+  export class Header extends Home {
     showModal = false;
 
     toggleModal(){
       this.showModal = !this.showModal;
+    }
+
+    form = new FormGroup(
+      {
+          authUser: new FormControl(),
+          password: new FormControl()
+      }
+    )
+  
+    login(e: Event) {
+      e.preventDefault();
+      
+      const { authUser, password} = this.form.value;
+  
+      this.http.post(`http://localhost:3000/login?login=${authUser}&password=${password}`, null)
+        .subscribe(data => {
+          console.log(data);
+          this.loged = true;
+      });
     }
 
     logout() {
@@ -22,4 +39,7 @@ import { AppComponent } from '../app.component';
       this.loged = false;
       this.showModal = false;
     }
+
+    faUser = faUser;
+    faRightToBracket = faRightToBracket;
   }
