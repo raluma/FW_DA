453e334710c14c7d8f9bdec86d1142e30241a223
Raúl Marrero Quevedo <ramaque01@gmail.com>
2023-03-22 18:14:51 +0000

    testing

--- frontend/src/app/controller/pages/home.ts
+++ frontend/src/app/controller/pages/home.ts
@@ -1,29 +1,14 @@
 import { Component } from '@angular/core';
 import { AppComponent } from '../app.component';
-import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'home',
   templateUrl: '../../view/pages/home.html'
 })
 export class Home extends AppComponent { 
-    form = new FormGroup(
-      {
-          authUser: new FormControl(""),
-          password: new FormControl()
-      }
-    )
-  
-    login(e: Event) {
-      e.preventDefault();
-      
-      const { authUser, password} = this.form.value;
-  
-      this.http.post(`http://localhost:3000/login?login=${authUser}&password=${password}`, null)
-        .subscribe((data) => {
-
-      });
-    }
+  showProfile() {
+    alert("Acceso al perfil");
+  }
 }
 
 
