779ecf5202eac39baed7dc6ec0465679a19098f0
Raúl Marrero Quevedo <ramaque01@gmail.com>
2023-03-28 19:04:25 +0100

    testing dynamic routes

--- frontend/src/app/controller/pages/event.ts
+++ frontend/src/app/controller/pages/event.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { AppComponent } from '../app.component';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'event',
   templateUrl: '../../view/pages/event.html'
 })
 export class Event extends AppComponent { 
-
+  constructor(public route: ActivatedRoute) {
+    super();
+    this.route.params.subscribe(params => console.log(params));
+  }
 }
 
 
