0178123914d03b8034e18cf42a7099b0ba57bdc5
Raúl Marrero Quevedo <ramaque01@gmail.com>
2023-03-28 18:19:24 +0100

    creating event page

---
+++ frontend/src/app/controller/pages/event.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { AppComponent } from '../app.component';
+
+@Component({
+  selector: 'event',
+  templateUrl: '../../view/pages/event.html'
+})
+export class Event extends AppComponent { 
+
+}
+
+
+ 
