cce8f3c09dbb8208d382ab2403c2711d123b323d
Raúl Marrero Quevedo <ramaque01@gmail.com>
2023-03-28 18:18:37 +0100

    declaring routes

--- frontend/src/app/controller/app-routing.module.ts
+++ frontend/src/app/controller/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Home } from './pages/home';
+import { Profile } from './pages/profile';
+import { Event } from './pages/event';
 
 const routes: Routes = [
   {
-    path: 'home',
+    path: '',
     component: Home
+  },
+  {
+    path: 'profile',
+    component: Profile
+  },
+  {
+    path: 'event',
+    component: Event
   }
 ];
 
