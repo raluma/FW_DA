2ce430f069620a01edf90be86079eb7802b01e0b
Raúl Marrero Quevedo <ramaque01@gmail.com>
2023-03-28 18:19:36 +0100

    creating profile page

---
+++ frontend/src/app/controller/pages/profile.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { AppComponent } from '../app.component';
+
+@Component({
+  selector: 'profile',
+  templateUrl: '../../view/pages/profile.html'
+})
+export class Profile extends AppComponent { 
+
+}
+
+
+ 
