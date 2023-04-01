bc7f65696fbcb46dc7c722875e870b856c23f473
Raúl Marrero Quevedo <ramaque01@gmail.com>
2023-04-01 11:34:46 +0100

    testing function

--- frontend/src/app/controller/app.component.ts
+++ frontend/src/app/controller/app.component.ts
@@ -9,6 +9,15 @@ export class AppComponent {
   http = inject(HttpClient);
 
   isLoged() {
-    return localStorage.getItem("authUser") !== null;
+    const authUser = localStorage.getItem('authUser');
+    const password = localStorage.getItem('password');
+    let loged = false;
+
+    this.http.post(`http://localhost:3000/login?login=${authUser}&password=${password}`, null)
+        .subscribe((obj : any) => {
+          if (obj['error'] === undefined) loged = true;
+    });
+
+    return loged;
   }
 }
