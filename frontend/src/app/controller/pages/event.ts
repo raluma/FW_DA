dec38c90a2f1d35eaa04a6609f13d884d037aae7
Raúl Marrero Quevedo <ramaque01@gmail.com>
2023-04-05 19:38:40 +0100

    creating view and logic of events

--- frontend/src/app/controller/pages/event.ts
+++ frontend/src/app/controller/pages/event.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
 import { AppComponent } from '../app.component';
 import { ActivatedRoute } from '@angular/router';
+import { faCalendar, faEdit } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'event',
   templateUrl: '../../view/pages/event.html'
 })
 export class Event extends AppComponent { 
+  faCalendar = faCalendar;
+  faEdit = faEdit;
+
+  action = "";
+
   constructor(public route: ActivatedRoute) {
     super();
-    this.route.params.subscribe(params => console.log(params));
+
+    this.route.params.subscribe(params => {
+      this.action = params['action'];
+    });
   }
 }
 
