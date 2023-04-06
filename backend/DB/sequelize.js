1bfb9519ad14c0061668a15dd867bc9ecc1bb146
Raúl Marrero Quevedo <ramaque01@gmail.com>
2023-04-06 12:08:44 +0100

    create new tables

--- backend/DB/sequelize.js
+++ backend/DB/sequelize.js
@@ -1,5 +1,5 @@
 import { Sequelize } from 'sequelize';
-import { user, event, exam } from './models.js';
+import { user, event, exam, work, leisure, appointment } from './models.js';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
@@ -15,8 +15,14 @@ export const sequelize = new Sequelize(DATABASE, USERNAME, PASSWORD, {
 export const User = sequelize.define('User', user);
 export const Event = sequelize.define('Event', event);
 export const Exam = sequelize.define('Exam', exam);
+export const Work = sequelize.define('Work', work);
+export const Leisure = sequelize.define('Leisure', leisure);
+export const Appointment = sequelize.define('Appointment', appointment);
 
 await User.sync({ alter: true });
 await Event.sync({ alter: true });
 await Exam.sync({ alter: true });
+await Work.sync({ alter: true });
+await Leisure.sync({ alter: true });
+await Appointment.sync({ alter: true });
 
