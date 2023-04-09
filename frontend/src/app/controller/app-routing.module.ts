b641247c5a91ac77d2644973083ead5dca4dedd7
Raúl Marrero Quevedo <ramaque01@gmail.com>
2023-04-09 17:52:15 +0100

    rename pages components

--- frontend/src/app/controller/app-routing.module.ts
+++ frontend/src/app/controller/app-routing.module.ts
@@ -1,30 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { Home } from './pages/home';
-import { Profile } from './pages/profile';
-import { Event } from './pages/event';
+import { HomePage } from './pages/home';
+import { ProfilePage } from './pages/profile';
+import { EventPage } from './pages/event';
 
 const routes: Routes = [
   {
     path: '',
-    component: Home
+    component: HomePage
   },
   {
     path: 'profile',
-    component: Profile
+    component: ProfilePage
   },
   {
-    path: 'event/:action/:datetime',
-    component: Event
+    path: 'event/:action/:tag/:datetime',
+    component: EventPage
   },
   {
     path: 'event/:action/:tag/:short_desc/:datetime',
-    component: Event
+    component: EventPage
   },
   { 
     path: '**', 
     pathMatch: 'full', 
-    component: Home
+    component: HomePage
   },
 ];
 
